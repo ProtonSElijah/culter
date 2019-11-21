20093ed1196dd0a940414fdcb932fd0f38a70373
ProtonSElijah <SElijah@protonmail.com>
2019-11-21 07:37:14 +0300

    Тест без ВК компонентов

---
+++ src/Test.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+/*import connect from '@vkontakte/vk-connect';
+import View from '@vkontakte/vkui/dist/components/View/View';*/
+import "./ResetBrowser.css";
+import "./Test.css";
+
+
+const Test = () => {
+
+    document.body.style.margin = 0;
+
+    return (
+        <div className="App">
+          <div className="App-header">
+            <p>Header</p>
+          </div>
+            <div className="Swipe-main">
+               <div className="Swipe-content">
+                  <div className="Swipe-content-up">
+                      <p>UP</p>
+                  </div>
+                  <div className="Swipe-content-down">
+                      <p id="Font-bold"><b>Элджей</b></p>
+                      <p>4 апреля</p>
+                      <p>20 человек</p>
+                  </div>
+               </div>
+            </div>
+          <div className="App-bottom">
+            <p>Footer</p>
+          </div>
+        </div>
+    );
+};
+
+export default Test;
