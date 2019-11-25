c78095808576216463b21b6eebeb30877b3955f2
ProtonSElijah <SElijah@protonmail.com>
2019-11-25 20:26:42 +0300

    Updates

--- precache-manifest.724aea1433a42d289adc3b0594bc96b3.js
+++ precache-manifest.724aea1433a42d289adc3b0594bc96b3.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00661537c00a47b21d26aa240a3ca774",
+    "revision": "b2da8136128e40af84a2cb11c76557e6",
     "url": "./index.html"
   },
   {
-    "revision": "c146c4bf847d620938b8",
-    "url": "./static/css/main.89c92adf.chunk.css"
+    "revision": "fbdfaa520e0b865ef4f1",
+    "url": "./static/css/main.9a926a27.chunk.css"
   },
   {
     "revision": "ef9ae0392a776b8c9129",
     "url": "./static/js/2.9f569b87.chunk.js"
   },
   {
-    "revision": "c146c4bf847d620938b8",
+    "revision": "fbdfaa520e0b865ef4f1",
     "url": "./static/js/main.93527a1d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3a5124526cd3ef1ce293ae80302349db.js"
+  "./precache-manifest.724aea1433a42d289adc3b0594bc96b3.js"
 );
 
 self.addEventListener('message', (event) => {
