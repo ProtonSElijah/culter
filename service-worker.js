be69963f845bc486803bc98c1514894f07c90709
ProtonSElijah <SElijah@protonmail.com>
2019-11-27 22:54:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0fd3e56bd878fe44d5a92ad7df5ff9b0.js"
+  "./precache-manifest.d2f2855119ff8277859696a8101deb74.js"
 );
 
 self.addEventListener('message', (event) => {
