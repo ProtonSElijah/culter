c3242b606f0ed32295aed8c238c00450c207e6db
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-01 21:13:28 +0300

    Первый вариант свайпов

---
+++ src/SwipeApp.js
@@ -0,0 +1,10 @@
+import {useSpring, animated, interpolate} from 'react-spring'
+import React from "react";
+import state from "react";
+import Deck from "./Components/Deck";
+
+const SwipeApp = () => {
+    return (<Deck/>);
+};
+
+export default SwipeApp;
