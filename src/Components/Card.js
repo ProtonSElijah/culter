e5dedbc387c95adff06f220d986dbc0c831e3223
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-06 01:13:18 +0300

    Добавлены свайпы без обновления конца свайпа и с неправильным растягиванием экрана

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,47 +1,102 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { animated, interpolate } from "react-spring";
-import Carousel from "nuka-carousel";
+import React, {Component} from 'react';
+import '../panels/panelsStyle/Swipe.css';
 
-class Card extends React.Component{
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentX: 0,
+            currentY: 0,
+            prevX: 0,
+            prevY: 0,
+            xDifference: 0,
+            startX: 0,
+            startY: 0,
+            xLength: 0,
+            yLength: 0,
+            isFirstTouch: true,
+            isSwiping: false
+        }
+    }
+
+    onTouch = (e) => {
+        e.stopPropagation();
+        let touches = e.changedTouches;
+        for (let i = 0; i < touches.length; i++){
+            let lastX = this.state.currentX;
+            let lastY = this.state.currentY;
+
+            let updatedState = {
+                prevX: lastX,
+                prevY: lastY,
+                currentX: touches[i].clientX,
+                currentY: touches[i].clientY,
+            };
+
+            if (this.state.isFirstTouch){
+                updatedState.isFirstTouch = false;
+            } else {
+                let xDifference = touches[i].clientX - lastX;
+                updatedState.xDifference = xDifference;
+                updatedState.xLength = this.state.xLength + xDifference;
+                let yDifference = touches[i].clientY - lastY;
+                updatedState.yDifference = yDifference;
+                updatedState.yLength = this.state.yLength + yDifference;
+            }
+
+            this.setState(updatedState);
+        }
+    };
+
+    onTouchStart = (e) => {
+        e.preventDefault();
+        this.setState({
+            isFirstTouch: true,
+            isSwiping: true
+        });
+    };
+    onTouchEnd = (e) => {
+        this.setState(
+            {
+                xLength: 0,
+                yLength: 0,
+                currentX: 0,
+                currentY: 0,
+                prevX: 0,
+                prevY: 0,
+                xDifference: 0,
+                yDifference: 0,
+                isSwiping: false
+            });
+    };
+
     render() {
-        const { i, x, y, rot, scale, trans, bind, data } = this.props;
-        const { name, age, distance, text, pics } = data[i];
+        let style = this.state.isSwiping
+            ?
+                {"margin-left": this.state.xLength*1.3 + "px",
+                "margin-top": this.state.yLength*0.9 + "px",
+                "transform": "rotate(" + -this.state.xLength / 20 + "deg)"}
+            :
+                {"margin": "2vmax 2.5vmin"};
         return (
-
-            <animated.div
-                key={i}
-                style={{
-                    transform: interpolate(
-                        [x, y],
-                        (x, y) => `translate3d(${x}px,${y}px,0)`
-                    )
-                }}
-            >
-                <animated.div
-                    {...bind(i)}
-                    style={{
-                        transform: interpolate([rot, scale], trans)
-                    }}
-                >
-                    <div className="card">
-                        <img src={pics[0]}/>
-                        <h2>{name},{age}</h2>
-                        <h5>{distance}</h5>
-                        <h5>{text}</h5>
+            <div className="Swipe-main">
+                <div className="Swipe-content"
+                     style={style}
+                     onTouchMove={this.onTouch}
+                     onTouchEnd={this.onTouchEnd}
+                     onTouchStart={this.onTouchStart}>
+                    <div className="Swipe-content-up">
+                        <img className="el" src="https://img.muz1.tv/img/2018-01-15/fmt_94_124_foto-1.jpg"/>
                     </div>
-                </animated.div>
-            </animated.div>
+                    <div className="Swipe-content-down">
+                        <p id="Font-bold"><b>Элджей</b></p>
+                        <p>11 апреля</p>
+                        <p>20 человек</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
-
-Card.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number,
-    distance: PropTypes.string,
-    text: PropTypes.string,
-    pics: PropTypes.array
-};
 
 export default Card;
--- src/panels/Swipe.js
+++ src/panels/Swipe.js
@@ -4,12 +4,9 @@ import Bottom from "../Components/Bottom";
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import '../ResetBrowser.css';
 import './panelsStyle/Swipe.css';
+import Card from "../Components/Card";
 
 const Swipe = ({id, go}) => {
-
-    const goTouch = e => {
-        alert("Работает " + e.type);
-    };
 
     useEffect(() => {
         async function refreshHeaderVK() {
@@ -22,18 +19,7 @@ const Swipe = ({id, go}) => {
         <Panel id={id}>
             <Header text={id}/>
 
-            <div className="Swipe-main">
-               <div className="Swipe-content">
-                  <div className="Swipe-content-up">
-                      <img className="el" src="https://img.muz1.tv/img/2018-01-15/fmt_94_124_foto-1.jpg" onTouchEnd={goTouch}/>
-                  </div>
-                  <div className="Swipe-content-down">
-                      <p id="Font-bold"><b>Элджей</b></p>
-                      <p>11 апреля</p>
-                      <p>20 человек</p>
-                  </div>
-               </div>
-            </div>
+            <Card/>
 
             <Bottom go={go} left="personal" right="matches"/>
         </Panel>
