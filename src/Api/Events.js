b57120df0b17d194b9d900b4ead372c88ee7586c
arseniy <molodeckij.b.2017@fml31.ru>
2019-12-23 20:19:10 +0300

    Обновлена работа с api - убраны лишние дебажные выводы и обновлена ссылка на сайт

--- src/Api/Events.js
+++ src/Api/Events.js
@@ -1,10 +1,7 @@
 import config from "./api_config"
 
-export async function fetchEvents(userId)  {
-    let url = config.url + "/user/" + userId + "/events/selection?category_id=1&category_id=6";
-    if (userId == "test"){
-        url = config.url + "/user/" + userId + "/events";
-    }
+export async function fetchEvents(userId,page = 0,size = 10,isPersonal=false)  {
+    let url = `${config.url}/user/${userId}/events/selection?category_id=1&category_id=6&page=${page}&size=${size}&is_personal=${isPersonal}`;
     return await fetch(url, {
         method: "GET",
     });
