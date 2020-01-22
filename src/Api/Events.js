13cd3eb6f128a7898e3a4110fd317822b912587b
arseniy <molodeckij.b.2017@fml31.ru>
2020-01-23 00:52:46 +0300

    обновлено апи для получения ивентов

--- src/Api/Events.js
+++ src/Api/Events.js
@@ -1,6 +1,6 @@
 import config from "./api_config"
 
 export async function fetchEvents(userId)  {
-    let url = config.url + "/user/" + userId + "/events/selection?category_id=1&category_id=6";
+    let url = config.url + "/user/" + userId + "/events/selection?category_id=1&category_id=6&is_personal=true";
     return fetch(url, {method: "GET",});
 }
