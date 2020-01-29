67a33584b8edace14bc2a4a047eda4e841c6672e
ProtonSElijah <SElijah@protonmail.com>
2020-01-29 03:50:17 +0300

    Added categories in filter modal panel

--- src/panels/Grid.js
+++ src/panels/Grid.js
@@ -14,6 +14,7 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 const Grid = ({id, go, user}) => {
 
     const [dataEvents, setDataEvents] = useState([]);
+    const [categoriesId, setCategoriesId] = useState([]);
 
     const uploadData = e => {
         let elem = e.currentTarget;
@@ -26,7 +27,6 @@ const Grid = ({id, go, user}) => {
             }
         }
     };
-
 
     async function loadEvents(){
         let eventsResponse = await fetchEvents(user.id);
@@ -55,7 +55,28 @@ const Grid = ({id, go, user}) => {
 
     const onModal = e => {
         let modal = document.getElementById("filter_modal");
+        if (modal.style.visibility == "visible") {
+            console.log(categoriesId);
+        }
         modal.style.visibility = (modal.style.visibility == "visible") ? "hidden" : "visible";
+    }
+
+    const onChangeCheckedCategories = e => {
+        /*if (e.target.checked) setCategoriesId(categoriesId.concat(e.target.value));
+        else categoriesId.splice(categoriesId.indexOf(e.target.value), 1);*/
+
+        if (e.target.checked) setCategoriesId(categoriesId.concat(e.target.value));
+        else categoriesId.splice(categoriesId.indexOf(e.target.value), 1);
+    }
+
+    const bb = e => {
+        if (e.currentTarget.dataset.isactive == "true") {
+            setCategoriesId(categoriesId.concat(e.currentTarget.dataset.id));
+        }
+        else categoriesId.splice(categoriesId.indexOf(e.currentTarget.dataset.id), 1);
+        e.currentTarget.dataset.isactive = (e.currentTarget.dataset.isactive == "true") ? "false" : "true";
+        e.currentTarget.children[0].classList.toggle("Filter-modal-categories-item-checkbox-disabled");
+        e.currentTarget.children[0].classList.toggle("Filter-modal-categories-item-checkbox-active");
     }
 
     return (
@@ -73,9 +94,59 @@ const Grid = ({id, go, user}) => {
 
                 <div className="Filter-modal" id="filter_modal" style={{visibility: 'hidden'}} >
                     <div className="Filter-modal-content">
+
                         <div className="Filter-modal-header">
                             <p>Фильтр</p>
                         </div>
+
+                        <div className="Filter-modal-categoriesTitle">
+                            <p>Выберите тип мероприятия</p>
+                            <div className="Filter-modal-categoriesTitleBottomLine"></div>
+                        </div>
+
+                        <div className="Filter-modal-categories">
+
+                            <div className="Filter-modal-categories-item"
+                               onClick={bb} data-isactive={true} data-id={31}>
+                                <div className="Filter-modal-categories-item-checkbox
+                                    Filter-modal-categories-item-checkbox-disabled"></div>
+                                <p className="Filter-modal-categories-item-name">Квесты</p>
+                            </div>
+
+                            <div className="Filter-modal-categories-item"
+                               onClick={bb} data-isactive={true} data-id={15}>
+                                <div className="Filter-modal-categories-item-checkbox
+                                    Filter-modal-categories-item-checkbox-disabled"></div>
+                                <p className="Filter-modal-categories-item-name">Флешмобы</p>
+                            </div>
+
+                            <div className="Filter-modal-categories-item"
+                               onClick={bb} data-isactive={true} data-id={6}>
+                                <div className="Filter-modal-categories-item-checkbox
+                                    Filter-modal-categories-item-checkbox-disabled"></div>
+                                <p className="Filter-modal-categories-item-name">Концерты</p>
+                            </div>
+
+                            <div className="Filter-modal-categories-item"
+                               onClick={bb} data-isactive={true} data-id={12}>
+                                <div className="Filter-modal-categories-item-checkbox
+                                    Filter-modal-categories-item-checkbox-disabled"></div>
+                                <p className="Filter-modal-categories-item-name">Выставки</p>
+                            </div>
+
+                            <div className="Filter-modal-categories-item"
+                               onClick={bb} data-isactive={true} data-id={27}>
+                                <div className="Filter-modal-categories-item-checkbox
+                                    Filter-modal-categories-item-checkbox-disabled"></div>
+                                <p className="Filter-modal-categories-item-name">Ночная жизнь</p>
+                            </div>
+
+                        </div>
+
+                        <div className="Filter-modal-buttonOK" onClick={onModal}>
+                            <p>ОК</p>
+                        </div>
+
                     </div>
                 </div>
 
