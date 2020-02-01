98396113f782b46c75e2593581dad4c2d45e0aef
arseniy <molodeckij.b.2017@fml31.ru>
2020-02-01 03:04:00 +0300

    добавлено апи для подгрузки людей

--- src/Api/People.js
+++ src/Api/People.js
@@ -7,3 +7,11 @@ export async function fetchPeople(userId, page=0, size=20)  {
         + "&size=" + size;
     return fetch(url, {method: "GET",});
 }
+
+export async function fetchMatches(userId, page=0, size=20) {
+    let url = config.url + "/user/" + userId 
+        + "/users/ratings?"
+        + "&page=" + page 
+        + "&size=" + size;
+    return fetch(url, {method: "GET",});
+}
