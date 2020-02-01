ce4a6d12f6ecaaf9808df05a6fa8c1dc1f521c71
arseniy <molodeckij.b.2017@fml31.ru>
2020-02-01 17:12:09 +0300

    fix rendering event without specified date

--- src/Components/View/EventInformationView.js
+++ src/Components/View/EventInformationView.js
@@ -3,6 +3,17 @@ import '../ComponentsStyle/CardView.scss'
 
 
 const EventInformationView = ({event}) => {
+    let title = event.short_title.length > 20 ?
+        event.short_title.slice(0,15) + "..." : event.short_title;
+
+    let isDateProvided = event.dates.length > 0;
+    if (!isDateProvided){
+        return  (
+            <div>
+                <p id="Font-bold">{title}</p>
+            </div>
+        )
+    }
     let time = event.dates[0].start_time.slice(0,event.dates[0].start_time.length-3);
 
     let date = new Date(event.dates[0].start_date);
@@ -10,20 +21,11 @@ const EventInformationView = ({event}) => {
     let week = date.toLocaleDateString("ru-Ru", { weekday: 'long' });
     let day = date.toLocaleDateString("ru-Ru", { month: 'long', day: 'numeric'});
 
-    let title = event.short_title.length > 20 ?
-        event.short_title.slice(0,15) + "..." : event.short_title;
-
     return  (
-
         <div>
             <p id="Font-bold">{title}</p>
-
-            <p> {day}</p>
-            <p>{time} {week} </p>
-
-
-
-
+                <p> {day}</p>
+                <p>{time} {week} </p>
         </div>
     )
 
