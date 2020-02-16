0b8340d5e66cd345c01ad127cce9eb6cfc7690ac
StomGramer3 <stomgramer3@stommarket.ru>
2020-02-16 17:56:49 +0300

    #28/feature-update-fonts (SF Pro Text)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './ComponentsStyle/Header.css';
 
-import IconApp from '../assets/icons/culterIcon.svg';
+import IconFilter from '../assets/icons/filter.svg';
 
 const Header = ({panelId}) => {
     let panelTitle = "";
     switch (panelId) {
         case "personal": panelTitle = "Профиль"; break;
         case "grid": panelTitle = "События"; break;
-        case "swipe": panelTitle = "События"; break;
+        case "events": panelTitle = "События"; break;
         case "people": panelTitle = "Люди"; break;
         case "matches": panelTitle = "Диалоги"; break;
     }
     return (
         <div className="App-header">
             <div className="App-header-content">
-               <img src={IconApp} alt="icon" />
                 <p>{"Culter"}</p>
             </div>
-            <div className="App-header-bottomLine"></div>
             <div className="App-header-panelTitle">
                 <p>{panelTitle}</p>
+                <img src={IconFilter} alt="filter" />
             </div>
         </div>
     );
