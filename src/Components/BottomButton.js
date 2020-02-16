31c4fc20a1c1f57e7b394c48f8ba253a2d645ce3
arseniy <molodeckij.b.2017@fml31.ru>
2020-02-16 15:33:15 +0300

    delete unused imports

--- src/Components/BottomButton.js
+++ src/Components/BottomButton.js
@@ -1,20 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './ComponentsStyle/Bottom.css';
-
-import chatDes from "../assets/icons/chatGrey.svg";
-import eventDes from "../assets/icons/peopleGrey.svg";
-import gridDes from "../assets/icons/gridGrey.svg";
-import swipeDes from "../assets/icons/eventsGrey.svg";
-import userDes from "../assets/icons/userGrey.svg";
-
-import chatActive from "../assets/icons/chatRed.svg";
-import eventActive from "../assets/icons/peopleRed.svg";
-import gridActive from "../assets/icons/gridRed.svg";
-import swipeActive from "../assets/icons/eventsRed.svg";
-import userActive from "../assets/icons/userRed.svg";
-import {changePanel} from "../redux/actions/panel-actions";
-import store from "../redux/store/store";
-import panels from "../panels.json";
 
 const BottomButton = ({panel, activePanel, activeImage, disabledImage, onClick }) => {
     return (
