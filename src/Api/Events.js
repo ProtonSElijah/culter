c4d4f6be8eba2de10c13dfcc4a412b401e5dab69
arseniy <molodeckij.b.2017@fml31.ru>
2020-03-02 01:02:31 +0300

    add api for retrieving common events

--- src/Api/Events.js
+++ src/Api/Events.js
@@ -16,11 +16,23 @@ export async function fetchEventsRequest(userId, categories, is_personal,
     return events;
 }
 
+export async function fetchCommonEventsRequest(userId, otherUserId, page, size) {
+    let url = buildUrlForCommonEvents(userId, otherUserId, page, size);
+    let response = await fetch(url, {method: "GET"});
+}
+
 function buildUrl(userId, categories, is_personal, page, size) {
     let categoriesParameter = "category_id=" + categories.join("&category_id=");
     return config.url + "/user/" + userId
         + "/events/selection?" + categoriesParameter
         + "&is_personal=" + is_personal
+        + "&page=" + page
+        + "&size=" + size;
+}
+
+function buildUrlForCommonEvents(userId, otherUserId, page, size) {
+    return config.url + "/user/" + userId
+        + "/common/events?" + "anotherUserId=" + otherUserId
         + "&page=" + page
         + "&size=" + size;
 }
