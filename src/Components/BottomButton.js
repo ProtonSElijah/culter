28ea357cbef968b2f3fd0222cc20a3fa01d5c1ea
StomGramer3 <stomgramer3@stommarket.ru>
2020-03-02 02:34:37 +0300

    added navigations button text

--- src/Components/BottomButton.js
+++ src/Components/BottomButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ComponentsStyle/Bottom.css';
 
-const BottomButton = ({panel, activePanel, activeImage, disabledImage, onClick }) => {
+const BottomButton = ({panel, activePanel, activeImage, disabledImage, onClick, text }) => {
     return (
         <div className="Bottom-button" onClick={onClick}
              data-to={panel}>
             <img src={activePanel === panel ? activeImage : disabledImage} alt={panel} />
+            <p className={activePanel === panel ? 'active' : ''}>{text}</p>
         </div>
     );
 };
