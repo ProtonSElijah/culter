2cfdffa92f39d784c63ed3687b04496c53603462
arseniy <molodeckij.b.2017@fml31.ru>
2020-03-14 21:33:28 +0300

    add updating user image if not exists

--- src/Api/Auth.js
+++
@@ -1,7 +0,0 @@
-import config from "./api_config"
-
-export async function authorizeRequest(user) {
-    let url = `${config.url}/user/identify?user_id=${user.id}`;
-    return fetch(url, {method: "GET",});
-
-}
