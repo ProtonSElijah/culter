c5e57615ff2e2180258f4418b12e1e4e16b6e702
arseniy <molodeckij.b.2017@fml31.ru>
2020-03-15 19:39:09 +0300

    add age rendering

--- src/Components/View/PersonInformationView.js
+++ src/Components/View/PersonInformationView.js
@@ -3,13 +3,23 @@ import '../ComponentsStyle/CardView.scss'
 
 const PersonInformationView = ({person}) => {
 
+    const getAge = (birthday) => {
+        let birthdayDate = new Date(birthday);
+        let ageDifMs = Date.now() - birthdayDate.getTime();
+        let ageDate = new Date(ageDifMs);
+        return  Math.abs(ageDate.getUTCFullYear() - 1970);
+    };
 
+    let age = "";
+    if (person.birthday){
+       age = getAge(person.birthday);
+    }
     return  (
 
         <div className="Swipe-content-down-container">
 
             <div className="Swipe-content-down-center user">
-                <p>{person.first_name}</p>
+                <p>{person.first_name}{age? `, ${age}` : ""}</p>
 
                 <p>Общих событий: <b>{person.count_common_events}</b></p>
    
