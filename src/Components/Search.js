5958420441915d6d3f12889b334bff95b178f53c
arseniy <molodeckij.b.2017@fml31.ru>
2020-04-22 21:45:04 +0300

    add basic search component

---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import store from "../redux/store/store";
+
+const Search = ({}) => {
+    return (
+        <div className="search-container">
+            <input type="text" placeholder="asd" />
+        </div>
+    )
+}
+
+export default Search;
