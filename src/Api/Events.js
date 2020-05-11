9308135127ba81876c5fe57f2238105c4afdea8b
arseniy <molodeckij.b.2017@fml31.ru>
2020-05-11 18:20:54 +0300

    add jsdocs to events.js api

--- src/Api/Events.js
+++ src/Api/Events.js
@@ -12,12 +12,19 @@ export async function fetchCommonEventsRequest(userId, otherUserId, page, size) 
   return await getPageableEventsByUrl(url);
 }
 
+/**
+ *
+ * @param {string} query
+ * @param {number} page pageable parameter for page number
+ * @param {number} size pageable parameter for page size
+ * @return {Promise<[]|any>} array of events
+ */
 export async function fetchEventsByQuery(query, page, size) {
   const url = buildUrlForEventsByQuery(query, page, size);
   return await getPageableEventsByUrl(url);
 }
 
-async function getPageableEventsByUrl(url) {
+const getPageableEventsByUrl = async (url) => {
   const response = await fetch(url, {method: 'GET'});
   if (!isValidResponse(response)) {
     return [];
@@ -29,35 +36,36 @@ async function getPageableEventsByUrl(url) {
 
   eventsJson.content = events;
   return eventsJson;
-}
+};
 
 
-function buildUrl(userId, categories, isPersonal, page, size) {
+const buildUrl = (userId, categories, isPersonal, page, size) => {
   const categoriesParameter = 'category_id=' + categories.join('&category_id=');
   return config.url + '/user/' + userId +
         '/events/selection?' + categoriesParameter +
         '&is_personal=' + isPersonal +
         '&page=' + page +
         '&size=' + size;
-}
+};
 
-function buildUrlForCommonEvents(userId, otherUserId, page, size) {
+
+const buildUrlForCommonEvents = (userId, otherUserId, page, size) => {
   return `${config.url}/user/${userId}` +
         `/common/events?anotherUserId=${otherUserId}` +
         '&page=' + page +
         '&size=' + size;
-}
+};
 
-function buildUrlForEventsByQuery(query, page, size) {
+const buildUrlForEventsByQuery = (query, page, size) => {
   return `${config.url}/event/search` +
     `?query=${query}`+ `&page=${page}` + `&size=${size}`;
-}
+};
 
 
-function updateImageUrls(newEvents) {
+const updateImageUrls = (newEvents) => {
   newEvents.forEach((item) => {
     item.images.forEach((imageItem) => {
       imageItem.image = config.images_url + imageItem.image;
     });
   });
-}
+};
